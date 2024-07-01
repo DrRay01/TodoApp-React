54d885792fc9fe9050c9bf592d3f09d2dceedf89
ATTAH RAYMOND KWAME <90375276+DrRay01@users.noreply.github.com>
2024-07-01 19:36:52 +0000

    update to my codes

--- todoapp/src/components/Form.jsx
+++ todoapp/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./form.module.css";
 export default function Form({ todos, setTodos }) {
   const [todo, setTodo] = useState("");
 
@@ -9,13 +10,19 @@ export default function Form({ todos, setTodos }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        onChange={(e) => setTodo(e.target.value)}
-        value={todo}
-        type="text"
-      />
-      <button type="submit">Add</button>
+    <form className={styles.todoform} onSubmit={handleSubmit}>
+      <div className={styles.container}>
+        <input
+          className={styles.modernInput}
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          type="text"
+          placeholder="enter todo item"
+        />
+        <button className={styles.addButton} type="submit">
+          Add
+        </button>
+      </div>
     </form>
   );
 }
--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -1,3 +1,15 @@
+import styles from "./todoItem.module.css";
 export default function TodoItem({ item }) {
-  return <h3 style={{ color: "red", fontSize: "3rem" }}>{item}</h3>;
+  return (
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        {item}{" "}
+        <span>
+          <button className={styles.deleteButton}>x</button>
+        </span>
+      </div>
+
+      <hr className={styles.line} />
+    </div>
+  );
 }
